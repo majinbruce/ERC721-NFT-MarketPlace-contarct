71d6c5baec1aa503256610f6831289a540dd20a3
omkar <omkar.c.344@gmail.com>
2022-05-23 11:22:33 +0530

    added ERC721 NFT token functionality

---
+++ contracts/ERC721.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLISENCED
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory NFTName, string memory NFTSymbol)
+        ERC721(NFTName, NFTSymbol)
+    {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}

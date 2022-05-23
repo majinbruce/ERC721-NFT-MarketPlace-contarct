8a8e2e0726d19041d1c6fee812b290256c3ee0fa
omkar <omkar.c.344@gmail.com>
2022-05-23 12:01:43 +0530

    added safe mint feature

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -23,7 +23,7 @@ contract MyNFT is ERC721URIStorage, Ownable {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
+        _safeMint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
     }

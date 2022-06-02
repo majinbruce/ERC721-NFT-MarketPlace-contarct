77de247a5aed4971891b8b4cac46d97ab4784511
omkar <omkar.c.344@gmail.com>
2022-06-02 10:19:40 +0530

    configured config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,41 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+const { RINKEBY_PRIVATE_KEY, ALCHEMY_API_KEY, ETHERSCAN_API_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+      },
+      {
+        version: "0.8.4",
+      },
+    ],
+  },
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+    },
+  },
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
